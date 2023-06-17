ac84b2de8a46d282f7da1cfe1a92a00e91b19c3a
stelios <stelios05@gmail.com>
2023-06-17 10:28:36 +0100

    spring initlzr generated

---
+++ src/main/java/io/github/steliospaps/spike/jpa/postgresql/events/SpikeJpaPostgresqlEventsApplication.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.spike.jpa.postgresql.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeJpaPostgresqlEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikeJpaPostgresqlEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/steliospaps/spike/jpa/postgresql/events/SpikeJpaPostgresqlEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.spike.jpa.postgresql.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikeJpaPostgresqlEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
